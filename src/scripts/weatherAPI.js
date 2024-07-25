3fe914ebd508be075fdd9963d3835b95a2460d29
Jonathan Wong <Xmexybutt@gmail.com>
2024-07-25 15:23:12 -0400

    Clean up

--- src/scripts/weatherAPI.js
+++ src/scripts/weatherAPI.js
@@ -4,31 +4,6 @@ const API_KEY = "de9f90ef3c71ac9d3628087d36d76ff5";
 const BASE_URL = "https://api.openweathermap.org/data/2.5/";
 
 const iconURL = (icon) => `https://openweathermap.org/img/wn/${icon}@2x.png`;
-// const GetCords = async (city) => {
-//   try {
-//     const response = await fetch(
-//       `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`,
-//       { mode: "cors" }
-//     );
-//     const data = await response.json();
-//     const coords = {
-//       lat: data[0].lat,
-//       lon: data[0].lon,
-//     };
-//     return coords;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-//Some old crappy code (this goes in getWeatherData) also change it to async to use this
-// const coords = await GetCords(cityName);
-// let lon, lat;
-// if (coords) {
-//   lon = coords.lon;
-//   lat = coords.lat;
-// }
-// url.search = new URLSearchParams({ lat: lat, lon: lon, appid: API_KEY });
 
 const getWeatherData = (infoType, searchParams) => {
   const url = new URL(BASE_URL + infoType);
